8e3211584b5368b54aea725011f304a5d0ef7cf7
arowley2 <arowleywork2025@gmail.com>
2025-08-24 16:55:02 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -57,32 +57,16 @@ model_time, h_arr, theta_arr = iterated_solver(mesh, V, params, min_x, max_x, nx
 crns_time = 60          # Pass '0' to turn off; 60 min is typical
 sat_time = 0            # Pass '0' to turn off; 1440 min is typical
 
-coll_t = 0
-while coll_t < 1:       # Change loop to match arrays in get_aot_params() in aot.py (default is [60, 30, 5, 1] min between observations)
-    coll_n = 0
-    while coll_n < 1:   # Change loop to match arrays in get_aot_params() in aot.py (default is [5, 7, 10, 12] observations nodes)
-        mu, collect_time, num_nodes = get_aot_params(dt, coll_t, coll_n)
-        aot_solver(mesh, V, params, min_x, max_x, nx, delta_x, aot_dt, T, dt_min, dt_max, h_0_dA, tol_dA, max_iter_dA, model_time, h_arr, theta_arr, collect_time, num_nodes, crns_time, sat_time)
-        coll_n = coll_n + 1
-    coll_t = coll_t + 1
+mu, collect_time, num_nodes = get_aot_params(dt, -1, -1)
+aot_solver(mesh, V, params, min_x, max_x, nx, delta_x, aot_dt, T, dt_min, dt_max, h_0_dA, tol_dA, max_iter_dA, model_time, h_arr, theta_arr, collect_time, num_nodes, crns_time, sat_time)
+
 
 # Find nudging approximate solution, with observation frequency (in min) given by collect_time and number of observation nodes given by num_nodes.
-coll_t = 0
-while coll_t < 1:       # Frequency and spatial values match those from AOT
-    coll_n = 0
-    while coll_n < 1:
-        mu, collect_time, num_nodes = get_aot_params(dt, coll_t, coll_n)
-        nudging_solver(mesh, V, params, min_x, max_x, nx, delta_x, dt, T, dt_min, dt_max, h_0_dA, 1, 50, model_time, h_arr, theta_arr, collect_time, num_nodes)
-        coll_n = coll_n + 1
-    coll_t = coll_t + 1
+mu, collect_time, num_nodes = get_aot_params(dt, -1, -1)
+nudging_solver(mesh, V, params, min_x, max_x, nx, delta_x, dt, T, dt_min, dt_max, h_0_dA, 1, 50, model_time, h_arr, theta_arr, collect_time, num_nodes)
+
 
 # Find Kalman approximate solution, with observation frequency (in min) given by collect_time and number of observation nodes given by num_nodes.
 num_ensembles = 200
-coll_t = 0
-while coll_t < 1:       # Frequency and spatial values match those from AOT
-    coll_n = 0
-    while coll_n < 1:
-        mu, collect_time, num_nodes = get_aot_params(dt, coll_t, coll_n)
-        kalman_solver(mesh, V, params, min_x, max_x, nx, delta_x, dt, T, dt_min, dt_max, h_0_dA, 1, 50, model_time, h_arr, theta_arr, collect_time, num_nodes, num_ensembles)
-        coll_n = coll_n + 1
-    coll_t = coll_t + 1
+mu, collect_time, num_nodes = get_aot_params(dt, -1, -1)
+kalman_solver(mesh, V, params, min_x, max_x, nx, delta_x, dt, T, dt_min, dt_max, h_0_dA, 1, 50, model_time, h_arr, theta_arr, collect_time, num_nodes, num_ensembles)
